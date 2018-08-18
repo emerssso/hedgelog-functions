c95c60956e2bb75242d504e568e3ae5e06870ec1
Conner Kasten <conner@emerssso.com>
2018-08-18 11:50:43 -0700

    publish temperature changes to pubsub

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,21 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as PubSub from '@google-cloud/pubsub';
 
 admin.initializeApp(functions.config().firebase);
+
+const pubsubClient = new PubSub({
+    projectId: process.env.GCLOUD_PROJECT,
+});
 
 /** This converts alerts posted by Hedgebase into push notifications for Hedgelog **/
 export const publishAlert = functions.firestore.document('alerts/{alertId}')
     .onWrite((snap, context) => {
-        if(!snap.after.exists) return -1;
+        if(!snap.after.exists) return 1;
 
         const alert = snap.after.data();
 
-        if (!alert.active) return 0;
+        if (!alert.active) return 2;
 
         const message = {
             notification: {
@@ -22,15 +27,37 @@ export const publishAlert = functions.firestore.document('alerts/{alertId}')
             topic: 'alerts'
         };
 
-        console.log("new alert with message: " + alert.message);
+        console.log(`new alert with message: ${alert.message}`);
 
         admin.messaging().send(message, false)
             .then((response) =>
-                console.log("message sent for alert: " + alert.message)
+                console.log(`message sent for alert: ${alert.message}`)
             )
             .catch((reason) =>
-                console.error("unable to send alert: " + reason)
+                console.error(`unable to send alert: ${reason}`)
             );
 
-        return 1;
+        return 0;
     });
+
+export const publishTemperature = functions.firestore.document('temperatures/current')
+    .onUpdate((change, context) => {
+        if(!change.after.exists) return 1;
+
+        const message = new Buffer(JSON.stringify(change.after.data()));
+
+        console.log(`Publishing temp message: message`);
+
+        pubsubClient
+            .topic('temperatures')
+            .publisher()
+            .publish(message)
+            .then(messageId => {
+                console.log(`Message ${messageId} published.`);
+            })
+            .catch(err => {
+                console.error('Unable to publish temp to pub/sub:', err);
+            });
+
+        return 0;
+});
